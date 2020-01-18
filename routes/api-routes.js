dc4cc1c0da965587c18ec1472944f1b6d92ebfa4
Neeraja Djearaman <neeru.jai@gmail.com>
2020-01-18 14:21:34 +0000

    added routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,79 +3,94 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
-  });
+    // Using the passport.authenticate middleware with our local strategy.
+    // If the user has valid login credentials, send them to the members page.
+    // Otherwise the user will be sent an error
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
+    });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    db.User.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password
+    // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+    // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+    // otherwise send back an error
+    app.post("/api/signup", function(req, res) {
+        db.User.create({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: req.body.password
+            })
+            .then(function() {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function(err) {
+                res.status(401).json(err);
+            });
+    });
+    app.post("/api/calculator", function(req, res) {
+        db.UserProfile.create({
+                age: req.body.age,
+                weight: req.body.weight,
+                gender: req.body.gender,
+                height: req.body.height
+
+            })
+            .then(function() {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function(err) {
+                res.status(401).json(err);
+            });
     })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
-  });
 
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
+    // Route for getting some data about our user to be used client side
+    app.get("/api/user_data", function(req, res) {
+        if (!req.user) {
+            // The user is not logged in, send back an empty object
+            res.json({});
+        } else {
+            // Otherwise send back the user's email and id
+            // Sending back a password, even a hashed password, isn't a good idea
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
+    });
 
-  //google oauth routes
-  app.get("/", (req, res) => {
-    if (req.session.token) {
-      res.cookie("token", req.session.token);
-      res.json({
-        status: "session cookie set"
-      });
-    } else {
-      res.cookie("token", "");
-      res.json({
-        status: "session cookie not set"
-      });
-    }
-  });
-  app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-      scope: ["https://www.googleapis.com/auth/userinfo.profile"]
+    //google oauth routes
+    app.get("/", (req, res) => {
+        if (req.session.token) {
+            res.cookie("token", req.session.token);
+            res.json({
+                status: "session cookie set"
+            });
+        } else {
+            res.cookie("token", "");
+            res.json({
+                status: "session cookie not set"
+            });
+        }
+    });
+    app.get(
+        "/auth/google",
+        passport.authenticate("google", {
+            scope: ["https://www.googleapis.com/auth/userinfo.profile"]
+        })
+    );
+    app.get(
+        "/auth/google/callback",
+        passport.authenticate("google", { failureRedirect: "/" }),
+        (req, res) => {
+            req.session.token = req.user.token;
+            res.redirect("/");
+        }
+    );
+
+    // Route for logging user out
+    app.get("/logout", function(req, res) {
+        req.logout();
+        // req.session = null;
+        res.redirect("/");
     })
-  );
-  app.get(
-    "/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "/" }),
-    (req, res) => {
-      req.session.token = req.user.token;
-      res.redirect("/");
-    }
-  );
-
-  // Route for logging user out
-  app.get("/logout", function(req, res) {
-    req.logout();
-    req.session = null;
-    res.redirect("/");
-  });
-};
+}
