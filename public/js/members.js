c7d2de3d34d475a9f17961f7087db0b8b942dd02
jennyunchan <jenniferyunchan@gmail.com>
2020-01-24 17:41:25 +0000

    amended members page

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+    $(".member-firstName").text(data.firstName);
+    $(".member-lastName").text(data.lastName);
+    $(".member-email").text(data.email);
+    $(".member-id").text(data.id);
+    $(".member-age").text(data.age);
+    $(".member-gender").text(data.gender);
+    $(".member-weight").text(data.weight);
+    $(".member-targetWeight").text(data.targetWeight);
   });
 });
